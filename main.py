a92ed54dc5f995b71ac98aa6af7118da0eb48eba
hungle45 <hung406474@gmail.com>
2022-10-21 16:33:41 +0700

    update free_play_menu UI

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-import pygame
-import pygame_menu
+import math
 import sys
 
+import pygame
+import pygame_menu
+
 # Global Variables
+CURRENT_STATE = 'MENU'
 
 # Global Constants
+DARK_BLUE = (16, 109, 191)
+LIGHT_BLUE = (33, 150, 243)
 FPS = 60
-COLOR_BACKGROUND = [15,125,117]
+NUMBER_OF_LEVELS = 33
+LEVEL_PER_ROW = 10
+COLOR_BACKGROUND = DARK_BLUE
 H_SIZE = 650  # Height of window size
 W_SIZE = 880  # Width of window size
 CUSTOME_THEME = pygame_menu.Theme(
     background_color=(255, 255, 255),
-    selection_color=(20, 136, 219),
-    title_background_color=(20, 136, 219),
+    selection_color=LIGHT_BLUE,
+    title_background_color=LIGHT_BLUE,
     title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_ADAPTIVE,
     title_font=pygame_menu.font.FONT_OPEN_SANS,
     title_font_antialias=True,
@@ -20,8 +27,8 @@ CUSTOME_THEME = pygame_menu.Theme(
     title_font_size=48,
     widget_font=pygame_menu.font.FONT_OPEN_SANS,
     widget_font_size=32,
-    widget_margin=(0,8)
-)
+    widget_margin=(0,8),
+    widget_cursor=pygame_menu.locals.CURSOR_HAND,)
 
 
 # Init pygame
@@ -36,13 +43,46 @@ surface = pygame.display.set_mode((W_SIZE,H_SIZE))
 
 # -----------------------------------------------------------------------------
 # Free play menu
+def free_map_chosen_level(level):
+    print(level)
+
+def free_map_level_chosen_btn_effect(is_select,widget,menu):
+    if is_select:
+        widget.set_font(font_size=32,color='white',
+                    font=pygame_menu.font.FONT_OPEN_SANS_BOLD,
+                    background_color=DARK_BLUE,readonly_color=LIGHT_BLUE,
+                    readonly_selected_color='white',selected_color='red',)
+    else:
+        widget.set_font(font_size=32,color='white',
+                    font=pygame_menu.font.FONT_OPEN_SANS_BOLD,
+                    background_color=LIGHT_BLUE,readonly_color=LIGHT_BLUE,
+                    readonly_selected_color='white',selected_color='red',)
+
+
 free_play_menu = pygame_menu.Menu('Bloxorz', W_SIZE, H_SIZE,
                                 onclose=None,
                                 theme=CUSTOME_THEME,
                                 mouse_motion_selection=True)
 
-free_play_menu.add.label('FREE PLAY',font_size=40)
-free_play_menu.add.button('BACK', pygame_menu.events.BACK)
+free_play_menu.add.label('LEVELS',font_size=40)
+
+# create level table structure
+for r_id in range(math.ceil(NUMBER_OF_LEVELS/LEVEL_PER_ROW)):
+    f = free_play_menu.add.frame_h( W_SIZE, 60, margin=(0,0))
+
+    for level_id in range(r_id*LEVEL_PER_ROW,
+    min((r_id+1)*LEVEL_PER_ROW,NUMBER_OF_LEVELS)):
+        btn = free_play_menu.add.button(f' {level_id+1:02d} ',
+                                        free_map_chosen_level,
+                                        level_id)
+        btn.set_margin(0, 0)
+        btn.set_padding((4,8))
+        btn.set_selection_effect(pygame_menu.widgets.NoneSelection())
+        btn.set_selection_callback(free_map_level_chosen_btn_effect)
+
+        f.pack(btn,align='align-center')
+
+free_play_menu.add.button('BACK', pygame_menu.events.BACK,font_size=24).translate(300,50)
 
 
 # -----------------------------------------------------------------------------
@@ -86,7 +126,7 @@ about_menu.add.button('BACK', pygame_menu.events.BACK)
 menu = pygame_menu.Menu('Bloxorz', W_SIZE, H_SIZE,
                         onclose=None,
                         theme=CUSTOME_THEME,
-                        mouse_motion_selection=True)
+                        mouse_motion_selection=True,)
 
 menu.add.button('PLAY GAME', play_menu)
 menu.add.button('ABOUT', about_menu)
