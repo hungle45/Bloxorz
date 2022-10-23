19a3d609d62ac0d53f5284da4350a0d9ca176962
hungle45 <hung406474@gmail.com>
2022-10-23 15:53:58 +0700

    add x_btn, o_btn; add level 2

--- game.py
+++ game.py
@@ -8,7 +8,9 @@ from problem import Action,Blozorx,State
 AVAILABLE_COLOR = (255, 255, 255)
 BLOCK_COLOR = (255,20,60)
 GOAL_COLOR = (50,205,50)
-# FLEXIBLE_CELL_COLOR = (242, 183, 5)
+BUTTON_COLOR = (105,105,105)
+FLEXIBLE_CELL_COLOR = (150,150,150)
+FRAGILE_CELL_COLOR = (242, 183, 5)
 GAME_COLOR_BACKGROUND = (0,0,139)
 
 
@@ -24,7 +26,7 @@ class Game:
         self.state = copy.deepcopy(self.problem.init_state)
 
         # drawing
-        self.square_size = min(50, (690-190)/max(self.problem.level.size_x,self.problem.level.size_y))
+        self.square_size = min(50, 700/max(self.problem.level.size_x,self.problem.level.size_y))
         self.starting_x = self.CENTER_X - self.problem.level.size_x / 2 * self.square_size
         self.starting_y = self.CENTER_Y - self.problem.level.size_y / 2 * self.square_size
         self.ending_x   = self.CENTER_X + self.problem.level.size_x / 2 * self.square_size
@@ -36,7 +38,7 @@ class Game:
         self.over = False # whether game is end
         self.ESC  = False
 
-    def process_input(self,events):
+    def process_input(self, events):
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if not self.over and event.key == pygame.K_LEFT:
@@ -59,11 +61,36 @@ class Game:
                 elif event.key == pygame.K_ESCAPE:
                     self.ESC = True
 
-    def _draw_sqr_cell(self,position,size,color):
+    def _draw_sqr_cell(self, position, size, color):
         pygame.draw.rect(
             self.surface,color,
             (*position,size,size)
         )
+
+    def _draw_x_btn_cell(self, position, size, cell_color, btn_color=BUTTON_COLOR):
+        self._draw_sqr_cell(position,size,cell_color)
+        cx = position[0]+size/2
+        cy = position[1]+size/2
+        btn_size = size*0.5*0.7
+        pygame.draw.line(self.surface,btn_color,(cx-btn_size,cy-btn_size),(cx+btn_size,cy+btn_size),int(size*0.2))
+        pygame.draw.line(self.surface,btn_color,(cx+btn_size,cy-btn_size),(cx-btn_size,cy+btn_size),int(size*0.2))
+
+
+    def _draw_o_btn_cell(self, position, size, cell_color, btn_color=BUTTON_COLOR):
+        self._draw_sqr_cell(position,size,cell_color)
+        cx = position[0]+size/2
+        cy = position[1]+size/2
+        pygame.draw.circle(self.surface,btn_color,(cx,cy),size*0.5*0.8)
+        
+    def _draw_splited_btn_cell(self, position, size, cell_color, btn_color=BUTTON_COLOR):
+        self._draw_sqr_cell(position,size,cell_color)
+        cx = position[0]+size/2
+        cy = position[1]+size/2
+        width = size*0.2
+        height = size*0.7
+        pygame.draw.line(self.surface,btn_color,(cx-width,cy-height/2),(cx-width,cy+height/2),int(width))
+        pygame.draw.line(self.surface,btn_color,(cx+width,cy-height/2),(cx+width,cy+height/2),int(width))
+
 
     def _draw_map(self):
         for x in range(self.problem.level.size_x):
@@ -72,11 +99,28 @@ class Game:
                 start_y = self.starting_y+1+y*self.square_size
 
                 if self.state.board_state[y,x]: # is available
-                    if self.problem.board[y,x,0] == Blozorx.CELL_TYPE_INT_MAP['normal']:
-                        self._draw_sqr_cell(
-                            position=(start_x,start_y),
-                            size=self.square_size-1,
-                            color=AVAILABLE_COLOR)
+                    cell_type = self.problem.board[y,x,0]
+
+                    if cell_type == Blozorx.CELL_TYPE_INT_MAP['normal']:
+                        self._draw_sqr_cell(position=(start_x,start_y),
+                            size=self.square_size-1,color=AVAILABLE_COLOR)
+
+                    elif cell_type == Blozorx.CELL_TYPE_INT_MAP['x_btn']:
+                        self._draw_x_btn_cell(position=(start_x,start_y),
+                            size=self.square_size-1,cell_color=AVAILABLE_COLOR)
+
+                    elif cell_type == Blozorx.CELL_TYPE_INT_MAP['o_btn']:
+                        self._draw_o_btn_cell(position=(start_x,start_y),
+                            size=self.square_size-1,cell_color=AVAILABLE_COLOR)
+
+                    elif cell_type == Blozorx.CELL_TYPE_INT_MAP['split_btn']:
+                        self._draw_splited_btn_cell(position=(start_x,start_y),
+                            size=self.square_size-1,cell_color=AVAILABLE_COLOR)
+
+                    if cell_type == Blozorx.CELL_TYPE_INT_MAP['flexible']:
+                        self._draw_sqr_cell(position=(start_x,start_y),
+                            size=self.square_size-1,color=FLEXIBLE_CELL_COLOR)
+
 
     def _draw_goal(self):
         start_x = self.starting_x+1+self.state.goal[1]*self.square_size
--- problem.py
+++ problem.py
@@ -7,6 +7,7 @@ class Action:
     RIGHT = 'RIGHT'
     UP    = 'UP'
     DOWN  = 'DOWN'
+    SWITCH = 'SWITCH'
 
 
 class State:
@@ -30,7 +31,7 @@ class State:
     def is_goal_state(self):
         return self.is_standing_state() \
             and self.cur == self.goal
-        
+
 
 class Blozorx:
     TRIGGER_TYPE_INT_MAP = {
@@ -50,9 +51,9 @@ class Blozorx:
     }
 
     def __init__(self, level_id):
-        self._load_level(level_id)
+        self.load_level(level_id)
 
-    def _load_level(self,level_id):
+    def load_level(self,level_id):
         self.level = Level(level_id)
 
         # board's cell: (cell_type, trigger_type)
@@ -67,24 +68,23 @@ class Blozorx:
         for x,y,trigger_type,trigger_num,target_list in self.level.x_btn_list:
             self.board[x,y,0] = self.CELL_TYPE_INT_MAP['x_btn']
             self.board[x,y,1] = self.TRIGGER_TYPE_INT_MAP[trigger_type]
-            for x,y in target_list:
-                self.board[x,y,0] = self.CELL_TYPE_INT_MAP['flexible']
+            for tx,ty in target_list:
+                self.board[tx,ty,0] = self.CELL_TYPE_INT_MAP['flexible']
             self.btn_target_map[(x,y)] = target_list
             btn_trigger_num[(x,y)] = trigger_num
 
         for x,y,trigger_type,trigger_num,target_list in self.level.o_btn_list:
             self.board[x,y,0] = self.CELL_TYPE_INT_MAP['o_btn']
             self.board[x,y,1] = self.TRIGGER_TYPE_INT_MAP[trigger_type]
-            for x,y in target_list:
-                self.board[x,y,0] = self.CELL_TYPE_INT_MAP['flexible']
+            for tx,ty in target_list:
+                self.board[tx,ty,0] = self.CELL_TYPE_INT_MAP['flexible']
             self.btn_target_map[(x,y)] = target_list
             btn_trigger_num[(x,y)] = trigger_num
 
-        for x,y,trigger_type,trigger_num,target_list in self.level.split_btn_list:
+        for x,y,trigger_num,target_list in self.level.split_btn_list:
             self.board[x,y,0] = self.CELL_TYPE_INT_MAP['split_btn']
-            self.board[x,y,1] = self.TRIGGER_TYPE_INT_MAP[trigger_type]
-            for x,y in target_list:
-                self.board[x,y,0] = self.CELL_TYPE_INT_MAP['flexible']
+            for tx,ty in target_list:
+                self.board[tx,ty,0] = self.CELL_TYPE_INT_MAP['flexible']
             self.btn_target_map[(x,y)] = target_list
             btn_trigger_num[(x,y)] = trigger_num
 
@@ -97,10 +97,7 @@ class Blozorx:
         possile_actions = [Action.UP,Action.DOWN,Action.LEFT,Action.RIGHT]
         return possile_actions
 
-    def do_action(self, state:State, action, inplace=False):
-        if not inplace:
-            state = copy.deepcopy(state)
-
+    def _move_block(self, state:State, action):
         if state.is_standing_state(): 
             x,y = state.cur
             if action == Action.UP:
@@ -111,7 +108,6 @@ class Blozorx:
                 state.cur = [x,y-2,x,y-1]
             elif action == Action.RIGHT:
                 state.cur = [x,y+1,x,y+2]
-
         elif state.is_lying_state():        
             x0,y0,x1,y1 = state.cur
             # lying on row
@@ -133,7 +129,6 @@ class Blozorx:
                     state.cur = [x0,y0-1,x1,y1-1]
                 elif action == Action.RIGHT:
                     state.cur = [x0,y0+1,x1,y1+1]
-
         # block is splited
         else:  
             x,y,_,_ = state.cur
@@ -146,6 +141,68 @@ class Blozorx:
                 pass
             elif action == Action.RIGHT:
                 pass
+
+    def _trigger_o_btn_if_possible(self, x, y, state:State):
+        if self.board[x,y,0] != self.CELL_TYPE_INT_MAP['o_btn']\
+            or state.btn_state[(x,y)] == 0: return
+
+        trigger_type = self.board[x,y,1]
+        if trigger_type == self.TRIGGER_TYPE_INT_MAP['hide']:
+            for tx,ty in self.btn_target_map[(x,y)]:
+                state.board_state[tx,ty] = False
+        if trigger_type == self.TRIGGER_TYPE_INT_MAP['unhide']:
+            for tx,ty in self.btn_target_map[(x,y)]:
+                state.board_state[tx,ty] = True
+        if trigger_type == self.TRIGGER_TYPE_INT_MAP['toggle']:
+            for tx,ty in self.btn_target_map[(x,y)]:
+                state.board_state[tx,ty] = not state.board_state[tx,ty]
+
+        if state.btn_state[(x,y)] != -1:
+            state.btn_state[(x,y)] -= 1
+    
+    def _trigger_x_btn_if_possible(self, x, y, state:State):
+        if self.board[x,y,0] != self.CELL_TYPE_INT_MAP['x_btn']\
+            or state.btn_state[(x,y)] == 0: return
+
+        trigger_type = self.board[x,y,1]
+        if trigger_type == self.TRIGGER_TYPE_INT_MAP['hide']:
+            for tx,ty in self.btn_target_map[(x,y)]:
+                state.board_state[tx,ty] = False
+        if trigger_type == self.TRIGGER_TYPE_INT_MAP['unhide']:
+            for tx,ty in self.btn_target_map[(x,y)]:
+                state.board_state[tx,ty] = True
+        if trigger_type == self.TRIGGER_TYPE_INT_MAP['toggle']:
+            for tx,ty in self.btn_target_map[(x,y)]:
+                state.board_state[tx,ty] = not state.board_state[tx,ty]
+                print(state.board_state[tx,ty])
+
+        if state.btn_state[(x,y)] != -1:
+            state.btn_state[(x,y)] -= 1
+
+
+    def _trigger_button(self, state):
+        print(state.cur)
+        if state.is_standing_state():
+            x,y = state.cur
+            self._trigger_o_btn_if_possible(x, y, state)
+            self._trigger_x_btn_if_possible(x, y, state)
+        elif state.is_lying_state():
+            x0,y0,x1,y1 = state.cur
+            self._trigger_o_btn_if_possible(x0, y0, state)
+            self._trigger_o_btn_if_possible(x1, y1, state)
+        else:
+            pass
+
+    def do_action(self, state:State, action, inplace=False):
+        if not inplace:
+            state = copy.deepcopy(state)
+
+        if action == Action.SWITCH:
+            if state.is_plited_state():
+                state.cur = state.cur[2:]+state.cur[:2]
+        else:
+            self._move_block(state, action)
+            self._trigger_button(state)
 
         if not inplace:
             return state
