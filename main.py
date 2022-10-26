05800cdf2dbe43e8274edb53eb567bdc2789b4e8
hungle45 <hung406474@gmail.com>
2022-10-26 23:17:13 +0700

    add algorithm menu

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from game import Game
 
 # Global Variables
 CURRENT_STATE = 'MENU'
+ALGORITHM = 'BFS'
 
 # Global Constants
 FPS = 60
@@ -50,16 +51,7 @@ clock = pygame.time.Clock()
 surface = pygame.display.set_mode((W_WIDTH_SIZE,W_HEIGHT_SIZE))
 
 
-# -----------------------------------------------------------------------------
-# Free play menu
-def free_map_chosen_level(level_id):
-    global GAME, CURRENT_STATE
-    menu.disable()
-    GAME = Game(surface, W_HEIGHT_SIZE, W_WIDTH_SIZE, level_id+1)
-    CURRENT_STATE = 'INGAME'
-
-
-def free_map_level_chosen_btn_effect(is_select,widget,menu):
+def level_chosen_btn_effect(is_select,widget,menu):
     if is_select:
         widget.set_font(font_size=32,color='white',
                     font=FONT_BOLD,
@@ -71,6 +63,14 @@ def free_map_level_chosen_btn_effect(is_select,widget,menu):
                     background_color=LIGHT_BLUE,readonly_color=LIGHT_BLUE,
                     readonly_selected_color='white',selected_color='red',)
 
+
+# -----------------------------------------------------------------------------
+# Free play menu
+def free_play_chosen_level(level_id):
+    global GAME, CURRENT_STATE
+    menu.disable()
+    GAME = Game(surface, W_HEIGHT_SIZE, W_WIDTH_SIZE, level_id+1)
+    CURRENT_STATE = 'INGAME'
 
 free_play_menu = pygame_menu.Menu('Bloxorz', W_WIDTH_SIZE, W_HEIGHT_SIZE,
                                 onclose=None,
@@ -86,12 +86,12 @@ for r_id in range(math.ceil(NUMBER_OF_LEVELS/LEVEL_PER_ROW)):
     for level_id in range(r_id*LEVEL_PER_ROW,
     min((r_id+1)*LEVEL_PER_ROW,NUMBER_OF_LEVELS)):
         btn = free_play_menu.add.button(f' {level_id+1:02d} ',
-                                        free_map_chosen_level,
+                                        free_play_chosen_level,
                                         level_id)
         btn.set_margin(0, 0)
         btn.set_padding((4,8))
         btn.set_selection_effect(pygame_menu.widgets.NoneSelection())
-        btn.set_selection_callback(free_map_level_chosen_btn_effect)
+        btn.set_selection_callback(level_chosen_btn_effect)
 
         f.pack(btn,align='align-center')
 
@@ -100,14 +100,43 @@ free_play_menu.add.button('BACK', pygame_menu.events.BACK,font_size=24).translat
 
 # -----------------------------------------------------------------------------
 # Algorithm menu
-algorithm_select_menu = pygame_menu.Menu('Bloxorz', W_WIDTH_SIZE, W_HEIGHT_SIZE,
+
+def algorithm_chosen_level(level_id):
+    print(ALGORITHM)
+
+def chosen_algorithm(selected_value, algorithm, **kwargs):
+    global ALGORITHM
+    ALGORITHM = algorithm
+
+algorithm_menu = pygame_menu.Menu('Bloxorz', W_WIDTH_SIZE, W_HEIGHT_SIZE,
                                 onclose=None,
                                 theme=CUSTOME_THEME,
                                 mouse_motion_selection=True)
 
-algorithm_select_menu.add.label('ALGORITHM SELECTION',font_size=40)
-algorithm_select_menu.add.button('BACK', pygame_menu.events.BACK)
+algorithm_menu.add.label('LEVELS',font_size=40)
 
+
+algorithm_menu.add.selector('Algorithm', 
+                            items=[('BFS','BFS'),('Generic','GA')],
+                            onchange=chosen_algorithm)
+
+# create level table structure
+for r_id in range(math.ceil(NUMBER_OF_LEVELS/LEVEL_PER_ROW)):
+    f = algorithm_menu.add.frame_h( W_WIDTH_SIZE, 60, margin=(0,0))
+
+    for level_id in range(r_id*LEVEL_PER_ROW,
+    min((r_id+1)*LEVEL_PER_ROW,NUMBER_OF_LEVELS)):
+        btn = algorithm_menu.add.button(f' {level_id+1:02d} ',
+                                        algorithm_chosen_level,
+                                        level_id)
+        btn.set_margin(0, 0)
+        btn.set_padding((4,8))
+        btn.set_selection_effect(pygame_menu.widgets.NoneSelection())
+        btn.set_selection_callback(level_chosen_btn_effect)
+
+        f.pack(btn,align='align-center')
+
+algorithm_menu.add.button('BACK', pygame_menu.events.BACK,font_size=24).translate(300,20)
 
 
 # -----------------------------------------------------------------------------
@@ -118,7 +147,7 @@ play_menu = pygame_menu.Menu('Bloxorz', W_WIDTH_SIZE, W_HEIGHT_SIZE,
                             mouse_motion_selection=True)
 
 play_menu.add.button('FREE PLAY', free_play_menu)
-play_menu.add.button('ALGORITHM', algorithm_select_menu)
+play_menu.add.button('ALGORITHM', algorithm_menu)
 play_menu.add.button('BACK', pygame_menu.events.BACK)
 
 
